3be7d84313e8ee356134a59f48d1c098ae35770d
Luka Zakrajsek <luka@bancek.net>
2018-03-10 15:20:24 +0100

    Upgrade yaml, add override config files, add env options

--- config.go
+++ config.go
@@ -2,37 +2,79 @@ package configutils
 
 import (
 	"fmt"
-	"github.com/koofr/envigo"
-	"gopkg.in/koofr/yaml.v1"
 	"io/ioutil"
+
+	"github.com/koofr/envigo"
+	yaml "gopkg.in/yaml.v2"
 )
+
+type LoadConfigOptions struct {
+	EnvOverride         bool
+	EnvPrefix           string
+	EnvGetter           envigo.EnvGetter
+	OverrideConfigFiles []string
+}
+
+func DisableEnvOverride() func(*LoadConfigOptions) {
+	return func(opts *LoadConfigOptions) {
+		opts.EnvOverride = false
+	}
+}
+
+func EnvPrefix(envPrefix string) func(*LoadConfigOptions) {
+	return func(opts *LoadConfigOptions) {
+		opts.EnvPrefix = envPrefix
+	}
+}
+
+func EnvGetter(envGetter envigo.EnvGetter) func(*LoadConfigOptions) {
+	return func(opts *LoadConfigOptions) {
+		opts.EnvGetter = envGetter
+	}
+}
+
+func OverrideConfigFile(configFile string) func(*LoadConfigOptions) {
+	return func(opts *LoadConfigOptions) {
+		opts.OverrideConfigFiles = append(opts.OverrideConfigFiles, configFile)
+	}
+}
 
 func LoadConfigFile(configFile string, config interface{}) (err error) {
 	configBytes, err := ioutil.ReadFile(configFile)
-
 	if err != nil {
-		return
+		return err
 	}
 
-	err = yaml.Unmarshal(configBytes, config)
-
-	return
+	return yaml.Unmarshal(configBytes, config)
 }
 
-func LoadConfig(configFile string, config interface{}) (err error) {
-	err = LoadConfigFile(configFile, config)
-
-	if err != nil {
-		err = fmt.Errorf("LoadConfig error: %s", err)
-		return
+func LoadConfig(configFile string, config interface{}, optFuncs ...func(*LoadConfigOptions)) (err error) {
+	opts := &LoadConfigOptions{
+		EnvOverride:         true,
+		EnvPrefix:           "",
+		EnvGetter:           envigo.EnvironGetter(),
+		OverrideConfigFiles: []string{},
 	}
 
-	err = envigo.Envigo(config, "", envigo.EnvironGetter())
-
-	if err != nil {
-		err = fmt.Errorf("LoadConfig envigo error: %s", err)
-		return
+	for _, optFunc := range optFuncs {
+		optFunc(opts)
 	}
 
-	return
+	if err = LoadConfigFile(configFile, config); err != nil {
+		return fmt.Errorf("LoadConfig error: %s", err)
+	}
+
+	for _, overrideConfigFile := range opts.OverrideConfigFiles {
+		if err = LoadConfigFile(overrideConfigFile, config); err != nil {
+			return fmt.Errorf("LoadConfig override error: %s", err)
+		}
+	}
+
+	if opts.EnvOverride {
+		if err = envigo.Envigo(config, opts.EnvPrefix, opts.EnvGetter); err != nil {
+			return fmt.Errorf("LoadConfig envigo error: %s", err)
+		}
+	}
+
+	return nil
 }
--- config_test.go
+++ config_test.go
@@ -1,32 +1,248 @@
 package configutils_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
 	. "github.com/koofr/go-configutils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"io/ioutil"
-	"os"
 )
 
+type Section struct {
+	SectionKey string
+}
+
 type Config struct {
-	Key string
+	Key     string
+	Do      bool
+	Pi      float64
+	Section *Section
+}
+
+const TestConfig = `
+key: "value"
+do: true
+pi: 3.14
+section:
+  sectionkey: "sectionvalue"
+`
+
+const OverrideConfig1 = `
+do: false
+`
+
+const OverrideConfig2 = `
+section:
+  sectionkey: sectionvalueoverride
+`
+
+const InvalidConfig = `
+key
+`
+
+func writeConfig(configFile string, content string) {
+	err := ioutil.WriteFile(configFile, []byte(content), 0600)
+	Expect(err).NotTo(HaveOccurred())
 }
 
 var _ = Describe("LoadConfig", func() {
-	It("should load config from YAML file", func() {
-		dir, err := ioutil.TempDir(os.TempDir(), "configutils-")
-		Expect(err).NotTo(HaveOccurred())
+	var tmp string
 
-		configFile := dir + "/config.yaml"
-
-		err = ioutil.WriteFile(configFile, []byte(`key: "value"`), 0600)
+	BeforeEach(func() {
+		var err error
+		tmp, err = ioutil.TempDir(os.TempDir(), "configutils-")
 		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should load config", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+		writeConfig(configFile, TestConfig)
 
 		cfg := &Config{}
 
-		err = LoadConfig(configFile, cfg)
+		err := LoadConfig(configFile, cfg)
 		Expect(err).NotTo(HaveOccurred())
 
-		Expect(cfg.Key).To(Equal("value"))
+		Expect(cfg).To(Equal(&Config{
+			Key: "value",
+			Do:  true,
+			Pi:  3.14,
+			Section: &Section{
+				SectionKey: "sectionvalue",
+			},
+		}))
+	})
+
+	It("should load config with env override", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+		writeConfig(configFile, TestConfig)
+
+		getenv := func(key string) (string, bool) {
+			switch key {
+			case "DO":
+				return "false", true
+			case "SECTION_SECTIONKEY":
+				return "sectionvalueoverride", true
+			default:
+				return "", false
+			}
+		}
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg, EnvGetter(getenv))
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(cfg).To(Equal(&Config{
+			Key: "value",
+			Do:  false,
+			Pi:  3.14,
+			Section: &Section{
+				SectionKey: "sectionvalueoverride",
+			},
+		}))
+	})
+
+	It("should load config with env override and prefix", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+		writeConfig(configFile, TestConfig)
+
+		getenv := func(key string) (string, bool) {
+			switch key {
+			case "MYAPP_DO":
+				return "false", true
+			case "MYAPP_SECTION_SECTIONKEY":
+				return "sectionvalueoverride", true
+			default:
+				return "", false
+			}
+		}
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg, EnvGetter(getenv), EnvPrefix("MYAPP"))
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(cfg).To(Equal(&Config{
+			Key: "value",
+			Do:  false,
+			Pi:  3.14,
+			Section: &Section{
+				SectionKey: "sectionvalueoverride",
+			},
+		}))
+	})
+
+	It("should load config without env override", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+		writeConfig(configFile, TestConfig)
+
+		getenv := func(key string) (string, bool) {
+			switch key {
+			case "DO":
+				return "false", true
+			case "SECTION_SECTIONKEY":
+				return "sectionvalueoverride", true
+			default:
+				return "", false
+			}
+		}
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg, EnvGetter(getenv), DisableEnvOverride())
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(cfg).To(Equal(&Config{
+			Key: "value",
+			Do:  true,
+			Pi:  3.14,
+			Section: &Section{
+				SectionKey: "sectionvalue",
+			},
+		}))
+	})
+
+	It("should load config with file override", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+		writeConfig(configFile, TestConfig)
+
+		overrideConfigFile1 := filepath.Join(tmp, "override1.yaml")
+		writeConfig(overrideConfigFile1, OverrideConfig1)
+
+		overrideConfigFile2 := filepath.Join(tmp, "override2.yaml")
+		writeConfig(overrideConfigFile2, OverrideConfig2)
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg, OverrideConfigFile(overrideConfigFile1), OverrideConfigFile(overrideConfigFile2))
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(cfg).To(Equal(&Config{
+			Key: "value",
+			Do:  false,
+			Pi:  3.14,
+			Section: &Section{
+				SectionKey: "sectionvalueoverride",
+			},
+		}))
+	})
+
+	It("should not load non-existent config", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(Equal(fmt.Sprintf("LoadConfig error: open %s: no such file or directory", configFile)))
+	})
+
+	It("should not load invalid config", func() {
+		configFile := filepath.Join(tmp, "invalidconfig.yaml")
+		writeConfig(configFile, InvalidConfig)
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("yaml: unmarshal errors"))
+	})
+
+	It("should not load invalid override config", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+		writeConfig(configFile, TestConfig)
+
+		overrideConfigFile := filepath.Join(tmp, "invalidconfig.yaml")
+		writeConfig(overrideConfigFile, InvalidConfig)
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg, OverrideConfigFile(overrideConfigFile))
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("yaml: unmarshal errors"))
+	})
+
+	It("should not load config with error in env variable", func() {
+		configFile := filepath.Join(tmp, "config.yaml")
+		writeConfig(configFile, TestConfig)
+
+		getenv := func(key string) (string, bool) {
+			switch key {
+			case "PI":
+				return "3,14", true
+			default:
+				return "", false
+			}
+		}
+
+		cfg := &Config{}
+
+		err := LoadConfig(configFile, cfg, EnvGetter(getenv))
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(Equal("LoadConfig envigo error: envigo PI parse float error: strconv.ParseFloat: parsing \"3,14\": invalid syntax"))
 	})
 })
